6ed97b29ebdf5fb2088a297718a88629d9c9ac2e
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-03-06 01:26:47 +0300

    Add files via upload

---
+++ 2.UNİTE UYGULAMA 17/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.UNİTE_UYGULAMA_17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool cevap_1 = radioButton1.Checked;
+            bool cevap_2 = radioButton2.Checked;
+
+            if (cevap_1 == true)
+            {
+                this.BackColor = Color.Black;
+                radioButton1.ForeColor = Color.White;
+                radioButton2.ForeColor = Color.White;
+
+            }
+            else
+            {
+                this.BackColor = Color.White;
+                radioButton1.ForeColor = Color.Black;
+                radioButton2.ForeColor = Color.Black;
+            }
+        }
+    }
+}
